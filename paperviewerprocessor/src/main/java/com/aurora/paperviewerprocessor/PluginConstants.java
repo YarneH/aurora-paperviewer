c25b9b74537cbf4a09eeda69727d6fcba48635b6
yarnehermann <yarne.hermann@ugent.be>
2019-05-03 19:56:59 +0200

    Make PluginConstant uninstantiatable

--- paperviewerprocessor/src/main/java/com/aurora/paperviewerprocessor/PluginConstants.java
+++ paperviewerprocessor/src/main/java/com/aurora/paperviewerprocessor/PluginConstants.java
@@ -4,6 +4,14 @@ package com.aurora.paperviewerprocessor;
  * Important Constants for the plugin
  */
 public class PluginConstants {
+
+    /**
+     * Prevent instantiation of utility class
+     */
+    private PluginConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
     /**
      * Unique name of the plugin, used by the caching operation.
      */
